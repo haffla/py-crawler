ea5d0456efdee01fdd2d79d47fb5c4a28207c51b
Jakob Pupke <jakob.pupke@gmail.com>
2015-04-29 11:23:36 +0200

    add tokenization and dictionary

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
 import urllib.request
+import re
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+
+class Tokenizer:
+
+    def tokenize(self, text):
+        words = re.split('\W+', text)
+        return words
+
+
+
+
+
 
 base_url = "http://people.f4.htw-berlin.de/fileadmin/user_upload/Dozenten/WI-Dozenten/Classen/DAWeb/smdocs/"
 
@@ -10,6 +22,8 @@ url_list = [base_url + site for site in sites]
 
 visited = []
 
+dictionary = {}
+
 def retrieveSite(url):
     with urllib.request.urlopen(url) as response:
         html = response.read()
@@ -17,21 +31,38 @@ def retrieveSite(url):
         return html
 
 def getLinksFromSite():
+    i = 0
     for url in url_list:
         if url not in visited:
             site = retrieveSite(url)
             soup = BeautifulSoup(site)
+
+            # get text from html document
+            text = soup.get_text()
+
+            #tokenize text
+            #normalized_tokens = [token.lower() for token in tokenizer.tokenize(text)][1:-1]
+
+            tokens = tokenizer.tokenize(text)
+
+            for t in tokens:
+                if(t not in dictionary):
+                    dictionary[t] = [i]
+                else:
+                    if(i not in dictionary[t]):
+                        dictionary[t].append(i)
+
             for link in soup.find_all('a'):
                 l = urllib.parse.urljoin(url, str(link.get('href')))
                 if l not in url_list:
                     url_list.append(l)
-                    print(l)
 
-    # just checkin'
-    print(len(url_list) == 8)
+            i = i + 1
 
 
+    print(dictionary)
 
 
 if __name__ == "__main__":
+    tokenizer = Tokenizer()
     getLinksFromSite()
