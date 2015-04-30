032138c4120a7bd63fc2fd8316f988ac186229dd
Jakob Pupke <jakob.pupke@gmail.com>
2015-04-30 12:24:22 +0200

    add main.py

--- src/crawling.py
+++ src/crawling.py
@@ -41,11 +41,3 @@ class Crawling(object):
                 # end of for loop over url_list
 
         return self.dictionary, self.url_list
-
-
-if __name__ == "__main__":
-    base = "http://people.f4.htw-berlin.de/fileadmin/user_upload/Dozenten/WI-Dozenten/Classen/DAWeb/smdocs/"
-    seed = ["d01.html", "d06.html", "d08.html"]
-    dicto, urls = Crawling(base, seed).get_links()
-    print(dicto)
-    print(urls)
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from src.crawling import Crawling
+
+if __name__ == "__main__":
+    base = "http://people.f4.htw-berlin.de/fileadmin/user_upload/Dozenten/WI-Dozenten/Classen/DAWeb/smdocs/"
+    seed = ["d01.html", "d06.html", "d08.html"]
+    dicto, urls = Crawling(base, seed).get_links()
+    print(dicto)
+    print(urls)
